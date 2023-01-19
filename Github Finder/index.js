25c1964864ee8e719c8543214386fae37cbd051a
avikodre03 <akodre111@gmail.com>
2023-01-19 19:45:20 +0530

    updated 1901

---
+++ Github Finder/index.js
@@ -0,0 +1,37 @@
+const searchInput = document.getElementById("searchBox")
+const profileContainer = document.getElementById("profileContainer")
+// console.log(profileContainer);
+
+async function func() {
+    const searchValue = searchInput.value
+    const Responce = await fetch(`https://api.github.com/search/users?q=${searchValue}`)
+    const data = await Responce.json()
+    console.log(data.items);
+
+    profileContainer.innerHTML = ""
+
+    data.items.map((ele) => {
+        const userName = ele.login
+        const avatar = ele.avatar_url
+        const profile = ele.html_url
+        console.log(userName, avatar, profile);
+
+        profileContainer.innerHTML += `
+        <div class="innerDiv">
+        <img src="${avatar}" >
+
+          <div>
+            <h3>${userName}</h3>
+             <a href="${profile}">View profile</a>
+           </div>
+
+        </div>
+        `
+    })
+}
+
+function clearFunc() {
+    profileContainer.innerHTML = ""
+    searchInput.value = ""
+
+}

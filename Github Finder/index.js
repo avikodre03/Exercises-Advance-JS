7e86379813455808d21232a4bd30a9e03da4e9d3
avikodre03 <akodre111@gmail.com>
2023-01-20 22:12:25 +0530

    updated 1901 n

--- Github Finder/index.js
+++ Github Finder/index.js
@@ -2,6 +2,34 @@ const searchInput = document.getElementById("searchBox")
 const profileContainer = document.getElementById("profileContainer")
 // console.log(profileContainer);
 
+async function func2() {
+    const Responce = await fetch("https://api.github.com/users")
+    // console.log(Responce);
+    const data = await Responce.json()
+
+    profileContainer.innerHTML = ""
+
+    data.map((ele) => {
+        const userName = ele.login
+        const avatar = ele.avatar_url
+        const profile = ele.html_url
+        // console.log(userName, avatar, profile);
+
+        profileContainer.innerHTML += `
+        <div class="innerDiv">
+        <img src="${avatar}" >
+
+          <div>
+            <h3>${userName}</h3>
+             <a href="${profile}">View profile</a>
+           </div>
+
+        </div>
+        `
+    })
+}
+func2()
+
 async function func() {
     const searchValue = searchInput.value
     const Responce = await fetch(`https://api.github.com/search/users?q=${searchValue}`)
@@ -33,5 +61,5 @@ async function func() {
 function clearFunc() {
     profileContainer.innerHTML = ""
     searchInput.value = ""
-
+    func2()
 }
---
+++ Quote Generator/index.js
@@ -0,0 +1,13 @@
+const quote = document.getElementById("quote")
+const auther = document.getElementById("auther")
+
+async function nextQuote() {
+
+    const Response = await fetch("https://api.quotable.io/random")
+    const data = await Response.json()
+    console.log(data);
+    quote.innerHTML=`<i class="fa-solid fa-quote-left"></i>${data.content}<i class="fa-solid fa-quote-right"></i>`
+    auther.innerHTML=`~ ${data.author}`
+}
+
+nextQuote()
